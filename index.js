4f0f778ce4f44dcb9b1805acc83799da69220159
Devangptl <devangmpatel51@gmail.com>
2024-05-18 10:47:00 +0530

    chang 1

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(express.json());
     host: process.env.HOST,
     user:process.env.USER,
     password:process.env.PASSWORD,
-    database:process.env.DATABASE
+    database:process.env.DATABASE,
+    port:process.env.PORT,
   })
 
 
